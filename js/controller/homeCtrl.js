6333900f91b0920ec263982caaa7544cf165b8d3
merroth <sj.emil@gmail.com>
2015-02-02 18:59:38 +0100

    Stærkere søge funktion
    søgefunktionen søger efter hele, eller dele af ord og sortere listen som
    sådan.

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -1,6 +1,8 @@
 angular
   .module('app') //Extends App
   .controller('homeCtrl', ['$scope', function($scope) {
+	  
+	$scope.homeIs ="";
 	 
 	$scope.menu = { //JSON objekt med menuen. "label" er den udtrykte tekst for menupunktet, "url" korresponderer med med "app.js" routen til undersiden uden ledende "/" og "keywords" er de nøgleord som søgefunktionen associerer med dette punkt.
 			home : {
@@ -22,6 +24,61 @@ angular
 			str+=arr[i]+" ";
 		}
 		return str;
+	}
+	$scope.sortMenu = function(query)
+	{
+		console.clear();
+		if(typeof(query) !== "string")
+		{
+			return $scope.menu;
+		}
+		
+		var warr = [],	//work array
+		rarr = [];	//return array
+		
+		query = query.split(" ")
+
+		for(var m in $scope.menu) // each keyword
+		{
+			var keys = $scope.menu[m].keywords;
+			var obj = {menu : $scope.menu[m], index : 0};
+			for(var i in query) // each search word
+			{
+				var q = query[i]
+				for(var z in keys)
+				{
+					var k = keys[z];
+					if(q==k)
+					{ obj.index++; }
+					if(k.lastIndexOf(q) >= 0)
+					{ obj.index++; }
+				}
+			}
+			warr.push(obj);
+		}
+
+		function returner(warr,rarr)
+		{
+			var best = { index : -1}
+			var latest = -1;
+			for(var i in warr)
+			{
+				if(warr[i].index > best.index)
+				{
+					best = warr[i];
+					latest = i;
+				}
+			}
+			rarr.push(best.menu);
+			warr.splice([latest],1);
+			
+			if(warr.length > 0)
+			{ rarr = returner(warr,rarr); }
+			return rarr;
+		}
+		rarr = returner(warr,rarr);
+		
+		return rarr;
 	}
 
   }]);
