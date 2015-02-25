903946af77485758dbb9db013b0449d3360db1b7
merroth <sj.emil@gmail.com>
2015-02-25 23:13:43 +0100

    Random Encounters - Terninge kaster
    En funktion til at kaste terninger

--- js/controller/encounterCtrl.js
+++ js/controller/encounterCtrl.js
@@ -4,6 +4,7 @@ angular
 {
 
 	$scope.zone = {value:0,name:"C"};
+	$scope.result = 0;
 	$scope.zones=[
 		{name:"AAA",value:8},
 		{name:"AA",value:6},
@@ -14,5 +15,59 @@ angular
 		{name:"E",value:-2},
 		{name:"Z",value:-4}
 	]
-
+	$scope.roll = function()
+	{
+		$scope.output = false;
+		var result1 ={
+		1 : 0,
+		2 : 0,
+		3 : 0,
+		4 : 0,
+		5 : 0,
+		6 : 0
+		},
+		result2 ={
+		1 : 0,
+		2 : 0,
+		3 : 0,
+		4 : 0,
+		5 : 0,
+		6 : 0
+		},
+		eyes = 0,
+		success = 0,
+		glitch = false;
+		
+		for(var i = 0;i<$scope.result;i++)
+		{
+			var x = Math.round(Math.random() * 5) + 1;
+			result1[x]++;
+			if(x >4)
+			{success++;}
+			
+		}
+		
+		for(var i = 0;i<6;i++)
+		{
+			var x = Math.round(Math.random() * 5) + 1;
+			result2[x]++;
+			eyes+=x;
+		}
+		if((result2[1]+result2[2]) > 2 && (result2[5]+result2[6]) < 1)
+		{
+			glitch = true;
+		}
+		$scope.output = {
+			die : $scope.result+0,
+			full : true,
+			success:success,
+			glitch:glitch,
+			resultone:result1,
+			resulttwo:result2,
+			eyes : eyes
+		};
+	}
+	$scope.output = {
+		full : false
+		};
 }]);
