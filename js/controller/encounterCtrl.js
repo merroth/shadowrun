414bdc0f8e6509499a3aeb8e5b57485af5e7d422
merroth <sj.emil@gmail.com>
2015-02-25 21:35:45 +0100

    Random Encounters tool
    Random Encounters udregner

--- js/controller/encounterCtrl.js
+++ js/controller/encounterCtrl.js
@@ -1,6 +1,18 @@
 angular
-  .module('app')
-  .controller('healingCtrl', ['$scope',  function($scope) {
-	
+.module('app')
+.controller('encounterCtrl', ['$scope',  function($scope)
+{
+
+	$scope.zone = {value:0,name:"C"};
+	$scope.zones=[
+		{name:"AAA",value:8},
+		{name:"AA",value:6},
+		{name:"A",value:4},
+		{name:"B",value:2},
+		{name:"C",value:0},
+		{name:"D",value:-1},
+		{name:"E",value:-2},
+		{name:"Z",value:-4}
+	]
 
 }]);
