ca6b10fe4ffb958acb075d63579a35d08df1e1e5
merroth <sj.emil@gmail.com>
2015-04-09 10:52:19 +0200

    Test ændring

--- js/app.js
+++ js/app.js
@@ -43,3 +43,40 @@ var masterApp=angular
   }])
   
 masterApp.query = "";
+
+masterApp.dieroller = function(cast)
+{
+	if(typeof(cast) == "undefined"){ cast = 1; }
+	var r ={
+		result : {
+			1 : 0,
+			2 : 0,
+			3 : 0,
+			4 : 0,
+			5 : 0,
+			6 : 0
+		},
+		eyes : 0,
+		success : 0,
+		glitch : false,
+		critGlitch : false
+	}
+	
+	for(var i = 0;i<cast;i++)
+	{
+		var x = Math.round(Math.random() * 5) + 1;
+		r.result[x]++;
+		r.eyes+=x;
+		if(x >4)
+		{r.success++;}
+	}
+	if((r.result[2] + r.result[1]) > 0 && (r.result[5] + r.result[6]) < Math.round(cast / 2))
+	{
+		r.glitch = true;
+		if((r.result[2] + r.result[1]) >= Math.round(cast / 2) && (r.result[5] + r.result[6]) == 0)
+		{
+			r.critGlitch = true;
+		}
+	}
+	return r;
+}
